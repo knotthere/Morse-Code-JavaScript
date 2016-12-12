0be2cb3208731952131058bf7c86ba32da3a963d
knottl <larry.knott@autodesk.com>
2016-12-11 16:53:15 -0800

    Output to a file

--- process.js
+++ process.js
@@ -3,26 +3,40 @@
 // and reformat it for use in:
 // https://github.com/Filupnot/morse-code
 
+const fs = require('fs');
+
 // This sucks in the JSON formatted file as data, ready for use
 var mc = require('./morse-code.json');
 
 //console.log(mc);
 
-// Now, iterate over each entty, and write the C++ code we want
+// Now, iterate over each entty, and writeSync the C++ code we want
 var strComment = "////////////////////////////////////////////////////////////////////////////////"
 
-console.log(strComment);
-for (var key in mc) {
-    // console.log(key, mc[key]);
-    var str = "mapMorse2Char[\"" + mc[key] + "\"] = \"" + key.toUpperCase() + "\";";
-    console.log(str);
-}
+fs.open('./mappings.cpp', 'w+', (err, fd) => {
+    //console.log(strComment);
+    fs.writeSync(fd, strComment);
+    fs.writeSync(fd, "\n");
+    for (var key in mc) {
+        // console.log(key, mc[key]);
+        var str = "mapMorse2Char[\"" + mc[key] + "\"] = \"" + key.toUpperCase() + "\";";
+        //console.log(str);
+        fs.writeSync(fd, str);
+        fs.writeSync(fd, "\n");
+    }
 
-console.log(strComment);
-// Let's make the reverse mapping'
-for (var key in mc) {
-    // console.log(key, mc[key]);
-    var str = "mapChar2Morse[\"" + key.toUpperCase() + "\"] = \"" + mc[key] + "\";";
-    console.log(str);
-}
-console.log(strComment);
+    fs.writeSync(fd, strComment);
+    fs.writeSync(fd, "\n");
+    //console.log(strComment);
+    // Let's make the reverse mapping'
+    for (var key in mc) {
+        // console.log(key, mc[key]);
+        var str = "mapChar2Morse[\"" + key.toUpperCase() + "\"] = \"" + mc[key] + "\";";
+        //console.log(str);
+        fs.writeSync(fd, str);
+        fs.writeSync(fd, "\n");
+  }
+    fs.writeSync(fd, strComment);
+    fs.writeSync(fd, "\n");
+    //console.log(strComment);
+});
